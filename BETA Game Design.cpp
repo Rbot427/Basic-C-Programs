5a4a65651bc17bfd30fbb86f3e6794cd1f0f7c4f
Rbot427 <rbot427@gmail.com>
2013-12-24 23:46:19 -0500

    Added AI

--- BETA Game Design.cpp
+++ BETA Game Design.cpp
@@ -50,10 +50,30 @@ public:
 	void printSquares();//Prints the squares[][] array
 	void drawPlayer();//Draws the player peice at a default of xSize / 2
 	void movePlayer(int direction);//Moves the player using the squares[][] array and glTranslatef().  Takes a direction
-	void drawAI();//In progress
+	void redrawAI();
 	~background();//In progress, when I figures out how to safely close a windows window
 
 };
+class player
+{
+public:
+	player();
+	void updatePos(int newX, int newY);
+	~player();
+	int y, x;
+};
+class AI
+{
+public:
+	void createAI();
+	void moveAI();
+	void printPos();
+	void updatePos(int newX, int newy);
+	~AI();
+	int x[3]; int y[3];
+};
+player myPlayer;
+AI myAI;
 background::background() {
 	for(int count = 0; count < ySize; count++)
 	{
@@ -67,8 +87,9 @@ void background::drawSquares() {
 	int isOb;
 	GLfloat mat_amb_diff[] = {1, 0, 0, 1};
 	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, mat_amb_diff);
+	cout << "\nCreating Walls \n";
 	srand(time(0));
-	glPushMatrix();
+    glPushMatrix();
 	glTranslatef(0, 0, 2.0);
 	glColor3f(1.0, 0.0, 0.0);
 	for(int count = 0; count < ySize; count++)
@@ -81,19 +102,20 @@ void background::drawSquares() {
 			{
 				if(count2 != xSize/2 && count != 0)//This if ensures a space to place the player
 				{
-					glutSolidCube(2.0);//Store where the squares are
-					squares[count2][count] = true;
+				glutSolidCube(2.0);//Store where the squares are
+				squares[count2][count] = true;
 				}
 				glTranslatef(2.0, 0, 0);
 			}
 			else
-				glTranslatef(2.0, 0, 0);
+			glTranslatef(2.0, 0, 0);
 		}
 		glPopMatrix();
 		glTranslatef(0, 2.0, 0);
 	}
 	glPopMatrix();
 	glFlush();
+	cout << "Done! \n";
 }
 void background::redrawSquares()
 {
@@ -109,8 +131,6 @@ void background::redrawSquares()
 		{
 			if(squares[count2][count] == 1)
 				glutSolidCube(2.0);
-			if(squares[count2][count] == 3)
-				glutSolidSphere(1, 50, 50);
 			glTranslatef(2, 0, 0);
 		}
 		glPopMatrix();
@@ -118,6 +138,15 @@ void background::redrawSquares()
 	}
 	glPopMatrix();
 	glFlush();
+}
+void background::redrawAI()
+{
+	GLfloat mat_amb_diff[] = {255/255, 36/255, 215/255, 1};
+	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, mat_amb_diff);
+	for(int counter = 0; counter < 3; counter++)
+	{
+		glTranslatef(myAI.x[counter] * 2, myAI.y[counter] * 2, 2); glutSolidSphere(1, 50, 50); glTranslatef(-myAI.x[counter] * 2, -myAI.y[counter] * 2, -2);
+	}
 }
 void background::drawBoard()
 {
@@ -143,7 +172,7 @@ void background::drawBoard()
 void background::newBoard()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
-	background();
+    background();
 	drawBoard();
 	drawSquares();
 }
@@ -172,10 +201,10 @@ void background::movePlayer(int direction)
 {
 	GLfloat mat_amb_diff[] = {0, 1, 0, 1};
 	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, mat_amb_diff);
-	static int x = xSize / 2, y = 0;
+	int x = myPlayer.x, y = myPlayer.y;
 	glClear(GL_COLOR_BUFFER_BIT);
 	glPushMatrix();
-	glTranslatef(x * 2, y * 2, 2);
+	glTranslatef(myPlayer.x * 2, myPlayer.y * 2, 2);
 	globalDirection = direction; //Might want to delete the global direction
 	switch(direction)
 	{
@@ -186,7 +215,7 @@ void background::movePlayer(int direction)
 			break;
 		}
 		glTranslatef(0, 2, 0); glutSolidSphere(1, 50, 50);
-		squares[x][y] = 0; squares[x][y + 1] = 2; y++; 
+		squares[x][y] = 0; myPlayer.updatePos(x, y + 1); 
 		break;
 	case LEFT:
 		if(squares[x-1][y] != 0 || x-1 < 0)
@@ -195,7 +224,7 @@ void background::movePlayer(int direction)
 			break;
 		}
 		glTranslatef(-2, 0, 0); glutSolidSphere(1, 50, 50);
-		squares[x][y] = 0; squares[x - 1][y] = 2; x += -1; 
+		squares[x][y] = 0; myPlayer.updatePos(x - 1, y);
 		break;
 	case RIGHT:
 		if(squares[x+1][y] != 0 || x+1 > xSize-1)//The xSize-1 adds a fix to the boundries
@@ -204,7 +233,7 @@ void background::movePlayer(int direction)
 			break;
 		}
 		glTranslatef(2, 0, 0); glutSolidSphere(1, 50, 50);
-		squares[x][y] = 0; squares[x + 1][y] = 2; x++;
+		squares[x][y] = 0; myPlayer.updatePos(x + 1, y);
 		break;
 	case DOWN:
 		if(squares[x][y-1] != 0 || y-1 < 0)
@@ -213,32 +242,14 @@ void background::movePlayer(int direction)
 			break;
 		}
 		glTranslatef(0, -2, 0); glutSolidSphere(1, 50, 50);
-		squares[x][y] = 0; squares[x][y - 1] = 2; y += -1; 
+		squares[x][y] = 0; myPlayer.updatePos(x, y - 1);
 		break;
 	default:
 		glutSolidSphere(1, 50, 50);
 		break;
 	}
 	glPopMatrix();
-}
-void background::drawAI()//TODO: Well, draw the AI
-{
-	GLfloat mat_amb_diff[] = {1, 0, 0, 1};
-	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, mat_amb_diff);
-	int counter = 0, posX = 0, posY = 0;
-	glPushMatrix(); srand(time(0));
-	while(counter != 3)
-	{
-		posX = rand() % xSize;
-		posY = rand() % ySize;
-		if(squares[posX][posY] == 0)
-		{
-			glTranslatef(posX * 2, posY * 2, 2);
-			glutSolidSphere(1, 50, 50); squares[posX][posY] = 3;
-			counter++; glTranslatef(-posX * 2, -posY * 2, -2);
-		}
-	}
-	glPopMatrix;
+	myAI.moveAI();
 }
 background::~background() {
 }
@@ -246,22 +257,138 @@ background::~background() {
 //Create an object entitled myBackground
 background myBackground;
 
+player::player()
+{
+	x = xSize / 2;
+	y = 0;
+}
+void player::updatePos(int newX, int newY)
+{
+	myBackground.squares[newX][newY] = 2;
+	x = newX; y = newY;
+}
+player::~player()
+{
+}
+void AI::createAI()
+{
+	GLfloat mat_amb_diff[] = {255/255, 36/255, 215/255, 1};
+	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, mat_amb_diff);
+	cout << "Creating AI... \n";
+	int counter = 0, posX = 0, posY = 0;
+	glPushMatrix(); srand(time(0));
+	while(counter != 3)
+	{
+		posX = rand() % xSize;
+		posY = rand() % ySize;
+		if(myBackground.squares[posX][posY] == 0)
+		{
+			x[counter] = posX; y[counter] = posY;
+			glTranslatef(posX * 2, posY * 2, 2);
+			glutSolidSphere(1, 50, 50); myBackground.squares[posX][posY] = 3;
+			counter++; glTranslatef(-posX * 2, -posY * 2, -2);
+		}
+	}
+	glPopMatrix;
+	cout << "Done!\n";
+
+}
+void AI::printPos() {
+	for(int count = 0; count < 3; count++)
+		cout << "\n" << x[count] << ", " << y[count] << "\n";
+}
+void AI::moveAI()//has to be called after a player movement
+{
+	GLfloat mat_amb_diff[] = {255/255, 36/255, 215/255, 1};
+	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, mat_amb_diff);
+	long float min = 10000000000, distance = 0;
+	int direction = NONE;
+	for(int counter = 0; counter < 3; counter++)
+	{
+		distance = sqrtf((myPlayer.y - y[counter])*(myPlayer.y - y[counter])) + ((myPlayer.x - x[counter])*(myPlayer.x - x[counter]));
+		if(distance < min){
+			direction = NONE;
+			min = distance;
+		}
+		distance = sqrtf((myPlayer.y - (y[counter]+1))*(myPlayer.y - (y[counter]+1))) + ((myPlayer.x - x[counter])*(myPlayer.x - x[counter]));
+		if(distance < min && y[counter] + 1 < ySize - 1 && myBackground.squares[x[counter]][y[counter]+1] != 1 && myBackground.squares[x[counter]][y[counter]+1] != 3) { 
+			direction = UP;
+			min = distance;
+		}
+		distance = sqrtf((myPlayer.y - y[counter])*(myPlayer.y - y[counter])) + ((myPlayer.x - (x[counter]-1))*(myPlayer.x - (x[counter]-1)));
+		if(distance < min && x[counter]-1 > -1 && myBackground.squares[x[counter]-1][y[counter]] != 1 && myBackground.squares[x[counter]-1][y[counter]] != 3) {
+			direction = LEFT;
+			min = distance;
+		}
+		distance = sqrtf((myPlayer.y - (y[counter]-1))*(myPlayer.y - (y[counter]-1))) + ((myPlayer.x - x[counter])*(myPlayer.x - x[counter]));
+		if(distance < min && y[counter] - 1 > -1 && myBackground.squares[x[counter]][y[counter]-1] != 1 && myBackground.squares[x[counter]][y[counter]-1] != 3) {
+			direction = DOWN;
+			min = distance;
+		}
+		distance = sqrtf((myPlayer.y - y[counter])*(myPlayer.y - y[counter])) + ((myPlayer.x - (x[counter]+1))*(myPlayer.x - (x[counter]+1)));
+		if(distance < min && x[counter] + 1 < xSize - 1 && myBackground.squares[x[counter]+1][y[counter]] != 1 && myBackground.squares[x[counter]+1][y[counter]] != 3) {
+			direction = RIGHT;
+			min = distance;
+		}
+		switch(direction) {
+		case NONE:
+			glTranslatef(x[counter] * 2, y[counter] * 2, 2);
+			glutSolidSphere(1, 50, 50);
+			glTranslatef(-x[counter] * 2, -y[counter] * 2, -2);
+			break;
+		case UP:
+			glTranslatef(x[counter] * 2, (y[counter]+1) * 2, 2);
+			glutSolidSphere(1, 50, 50);
+			glTranslatef(-x[counter] * 2, -(y[counter] + 1) * 2, -2);
+			myBackground.squares[x[counter]][y[counter]] = 0; y[counter]++; myBackground.squares[x[counter]][y[counter]] = 3;
+			break;
+		case LEFT:
+			glTranslatef((x[counter]-1) * 2, y[counter] * 2, 2);
+			glutSolidSphere(1, 50, 50);
+			glTranslatef(-(x[counter]-1) * 2, -y[counter] * 2, -2);
+			myBackground.squares[x[counter]][y[counter]] = 0; x[counter] += -1; myBackground.squares[x[counter]][y[counter]] = 3;
+			break;
+		case DOWN:
+			glTranslatef(x[counter] * 2, (y[counter]-1) * 2, 2);
+			glutSolidSphere(1, 50, 50);
+			glTranslatef(-x[counter] * 2, -(y[counter]-1) * 2, -2);
+			myBackground.squares[x[counter]][y[counter]] = 0; y[counter]+= -1; myBackground.squares[x[counter]][y[counter]] = 3;
+			break;
+		case RIGHT:
+			glTranslatef((x[counter]+1) * 2, y[counter] * 2, 2);
+			glutSolidSphere(1, 50, 50);
+			glTranslatef(-(x[counter]+1) * 2, -y[counter] * 2, -2);
+			myBackground.squares[x[counter]][y[counter]] = 0; x[counter]++; myBackground.squares[x[counter]][y[counter]] = 3;
+			break;
+		}
+		min = 10000000000000;
+	}
+}
+void AI::updatePos(int newX, int newY) {
+	
+}
+AI::~AI()
+{
+}
 void display() {
 	glClear(GL_COLOR_BUFFER_BIT);
 	glClear(GL_DEPTH_BUFFER_BIT);
 	glClearColor(0.0, 0.0, 0.0, 0.0);
 	if(start)
 	{
-		myBackground.drawSquares();
-		myBackground.drawPlayer();
-		myBackground.drawAI();
-		start = false;
+	myBackground.drawSquares();
+	myBackground.drawPlayer();
+	myAI.createAI();
+	start = false;
 	}
 	else
-		myBackground.movePlayer(globalDirection);
+	{
+		myBackground.movePlayer(globalDirection); myBackground.redrawAI();
+	}	
 	myBackground.drawBoard();
 	myBackground.redrawSquares();
 
+	
 	glFlush();
 }
 void getGlobal()//Don't need this function anymore
@@ -274,37 +401,37 @@ void keyboard_handler(unsigned char c,int xi,int yi){
 	case 'g':
 		myBackground.printSquares();
 		break;
-	case 'w' :
-		globalY++;
-		init();
-		break;
-	case 'a' :
-		globalX--;
-		init();
-		break;
-	case 's' :
-		globalY--;
-		init();
-		break;
-	case 'd':
-		globalX++;
-		init();
-		break;
-	case 'e':
-		globalZ++;
-		init();
-		break;
-	case 'q':
-		globalZ--;
-		init();
-		break;
-	case 'r':
-		system("cls");
-		break;
-		//case 'l':
-		// loadConfig();
-		// break;
-	}   
+		case 'w' :
+		  globalY++;
+		 init();
+	break;
+      case 'a' :
+		  globalX--;
+		 init();
+	break;
+      case 's' :
+		  globalY--;
+		  init();
+	break;
+	  case 'd':
+		  globalX++;
+		 init();
+	break;
+	  case 'e':
+		  globalZ++;
+		  init();
+		  break;
+	  case 'q':
+		  globalZ--;
+		  init();
+		  break;
+	  case 'r':
+		  system("cls");
+		  break;
+	  //case 'l':
+		 // loadConfig();
+		 // break;
+    }   
 	globalDirection = NONE;
 }
 void special_key_handler(int key,int xi,int yi) 
@@ -328,7 +455,7 @@ void special_key_handler(int key,int xi,int yi)
 }
 
 void init() {
-	glClearColor(0.8, 0.8, 0, 0);
+    glClearColor(0.8, 0.8, 0, 0);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	gluPerspective(60, 1, 1, 200);
@@ -338,7 +465,7 @@ void init() {
 	gluLookAt(globalX, globalY, globalZ, xSize, ySize, 0, 0, 1, 0);
 
 	//lighting
-
+	
 	glColor3f(1, 0, 0);
 	GLfloat mat_specular[] = {1.0, 1.0, 1.0, 1.0};
 	GLfloat mat_shininess[] = {50.0};
@@ -363,30 +490,32 @@ void prog_cont() {
 /*
 bool loadConfig()
 {
-FILE *fp1; char c = 0; stringstream XSize;
-if(DOIT){
-fp1 = fopen("config.txt", "rb");
-while(c != ':')
-c = fgetc(fp1);
-for(int count = 0; c != 'Y'; count++){
-cout << "C is " << c;
-c = fgetc(fp1);
-XSize << c;
-}
-int result;
-XSize >> result;
-cout << result;
-return true;
-}
-else
-return false;
+	FILE *fp1; char c = 0; stringstream XSize;
+	if(DOIT){
+	fp1 = fopen("config.txt", "rb");
+	while(c != ':')
+		c = fgetc(fp1);
+	for(int count = 0; c != 'Y'; count++){
+		cout << "C is " << c;
+		c = fgetc(fp1);
+		XSize << c;
+	}
+		int result;
+		XSize >> result;
+		cout << result;
+		return true;
+	}
+	else
+		return false;
 }
 */
 int main(int argc, _TCHAR* argv[])
 {
+	cout << "Starting up GLUT... \n";
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
 	glutInitWindowPosition(0, 0);
 	glutInitWindowSize(500, 500);
+	cout << "Done!";
 	glutCreateWindow("RUN!!!");
 	init();
 
